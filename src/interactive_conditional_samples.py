3c29c61c1697d4be5d3c30ceaf317a883ffc2c1d
iliusa77 <iliacomarov@gmail.com>
2020-11-06 15:10:20 +0200

    Update interactive_conditional_samples.py

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -4,8 +4,8 @@ import fire
 import json
 import os
 import numpy as np
-#import tensorflow as tf
-from tensorflow.python.compiler import tensorrt as tf
+import tensorflow as tf
+
 
 import model, sample, encoder
 
